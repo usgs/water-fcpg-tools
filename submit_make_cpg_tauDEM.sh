7a07100d51dff6d6cf90f666ba4ce4b84aec04a4
Theodore Barnhart <tbarnhart@usgs.gov>
2018-10-23 10:05:53 -0600

    added more time for larger areas

--- submit_make_cpg_tauDEM.sh
+++ submit_make_cpg_tauDEM.sh
@@ -5,7 +5,7 @@
 #SBATCH --tasks-per-node=16
 #SBATCH -p normal                         # the partition you want to use, for this case prod is best
 #SBATCH --account=wymtwsc        # your account
-#SBATCH --time=04:00:00           # Overestimated guess at time
+#SBATCH --time=12:00:00           # Overestimated guess at time
 #SBATCH --mail-type=ALL         # Send email on all events
 #SBATCH --mail-user=tbarnhart@usgs.gov
 #SBATCH -o %j.log                    # Sets output log file to %j ( will be the jobId returned by sbatch)  
