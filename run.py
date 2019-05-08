f52c40a75c7fad79a7ac448f8025b5d11e82ca4d
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-05-08 14:19:35 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 from tools import *
 
+
+jobID = sys.argv[1] # pull the slurm job ID
+cores = int(sys.argv[2]) # pull the number of cores available
+
+print("jobID:%s"%jobID)
+print("Available Cores:%s"%cores)
+
 #Inputs
 fdr = "../100500010101b/fdr100500010101b.tif"
 fac = "../100500010101b/fac100500010101b.tif"
@@ -15,7 +22,7 @@ CPG = "../100500010101b/work/elevCPG100500010101b.tif"
 
 tauDrainDir(fdr, taufdr)
 
-accumulateParam(paramRast, taufdr, accumParam, cores = 10)
+accumulateParam(paramRast, taufdr, accumParam, cores)
 
 
 make_cpg(accumParam, fac, CPG)
