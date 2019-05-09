e555ff72cff280f9c4f8c80353c21497919256b9
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-05-09 16:23:45 +0000

    Update testResample.py

--- testResample.py
+++ testResample.py
@@ -4,6 +4,6 @@ from tools import *
 inParam = "../100500010101/dem100500010101.tif"
 fdr = "../100500010101/fdr100500010101.tif"
 outParam = "../100500010101/rs100500010101.tif"
-resampleMethod = 1
+resampleMethod = Resampling.bilinear
 
 resampleParam(inParam, fdr, outParam, resampleMethod)
