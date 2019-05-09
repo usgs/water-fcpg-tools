315b08c7cb219eab54621eec129b7200ac00b759
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-05-09 18:52:43 +0000

    Update tools.py

--- tools.py
+++ tools.py
@@ -229,8 +229,8 @@ def resampleParam(inParam, fdr, outParam, resampleMethod):
 
     with rs.open(inParam) as src: # load accumulated data and no data rasters
         trans, w, h = calculate_default_transform(src.crs, fdrcrs, src.width, src.height, *src.bounds
-        inParamRaster = ds.read(1)
-        profile = ds.profile.copy()
+        inParamRaster = src.read(1)
+        profile = src.profile.copy()
 
         profile.update({
                 'profile':'GeoTIFF',
