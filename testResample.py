da2b1d985476978f1da075319f13cc3ec93cb5f3
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-05-09 16:26:54 +0000

    Update testResample.py

--- testResample.py
+++ testResample.py
@@ -4,6 +4,6 @@ import rasterio as rs
 inParam = "../100500010101/dem100500010101.tif"
 fdr = "../100500010101/fdr100500010101.tif"
 outParam = "../100500010101/rs100500010101.tif"
-#resampleMethod = rs.Resampling.bilinear
+resampleMethod = 1
 
 resampleParam(inParam, fdr, outParam, resampleMethod)
