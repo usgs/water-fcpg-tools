8a49fa31a541bfbbebc70e229702755c692d439b
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-05-09 19:18:45 +0000

    Update testResample.py

--- testResample.py
+++ testResample.py
@@ -4,6 +4,6 @@ import rasterio as rs
 inParam = "../100500010101/2015PRISM_100500010101.tif"
 fdr = "../100500010101/fdr100500010101.tif"
 outParam = "../100500010101/PRISM100500010101.tif"
-resampleMethod = 1
+resampleMethod = "nearest"
 
 resampleParam(inParam, fdr, outParam, resampleMethod)
