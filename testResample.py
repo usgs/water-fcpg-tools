7e9fdbcb34115f7aabfc369942d74c707e378609
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-05-09 15:53:00 +0000

    Put quotes around file paths

--- testResample.py
+++ testResample.py
@@ -1,9 +1,9 @@
 from tools import *
 
 
-inParam = ../100500010101/dem100500010101.tif
-fdr = ../100500010101/fdr100500010101.tif
-outParam = ../100500010101/rs100500010101.tif
+inParam = "../100500010101/dem100500010101.tif"
+fdr = "../100500010101/fdr100500010101.tif"
+outParam = "../100500010101/rs100500010101.tif"
 resampleMethod = 1
 
 resampleParam(inParam, fdr, outParam, resampleMethod)
