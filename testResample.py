936b751cd39e933c0950b79253f14a5129a14f34
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-05-09 17:56:45 +0000

    Update testResample.py

--- testResample.py
+++ testResample.py
@@ -1,9 +1,9 @@
 from tools import *
 import rasterio as rs
 
-inParam = "../100500010101/dem100500010101.tif"
+inParam = "../1005/2015PRISM_Milk.tif"
 fdr = "../100500010101/fdr100500010101.tif"
-outParam = "../100500010101/rs100500010101.tif"
+outParam = "../100500010101/PRISM100500010101.tif"
 resampleMethod = 1
 
 resampleParam(inParam, fdr, outParam, resampleMethod)
