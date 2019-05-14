7f29c6fddbe5174f66eee2c43a890808aa9e1f15
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-05-14 15:22:57 +0000

    Catch errors from invalid system inputs.

--- runPRISMCPG.py
+++ runPRISMCPG.py
@@ -4,13 +4,19 @@ print(sys.argv)
 
 cores = 1 #Default number of cores to use
 
-if len(sys.argv == 3):
+try:
+    if len(sys.argv == 3):
 
-    jobID = sys.argv[1] # pull the slurm job ID
-    cores = int(sys.argv[2]) # pull the number of cores available
+        jobID = sys.argv[1] # pull the slurm job ID
+        cores = int(sys.argv[2]) # pull the number of cores available
 
-    print("jobID:%s"%jobID)
-    print("Available Cores:%s"%cores)
+        print("jobID:%s"%jobID)
+        print("Available Cores:%s"%cores)
+
+except TypeError:
+    print("Number of Cores set to 1")
+    pass
+
 
 #Inputs
 fdr = "../1005/fdr1005.tif"
