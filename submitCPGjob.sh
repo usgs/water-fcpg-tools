0deae83647a9369b99365664d734cf4f29ae59ab
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-05-14 16:41:48 +0000

    Update submitCPGjob.sh

--- submitCPGjob.sh
+++ submitCPGjob.sh
@@ -11,10 +11,10 @@
 #SBATCH -o %j.log                    # Sets output log file to %j ( will be the jobId returned by sbatch)  
 #SBATCH --mem=128000            #memory in MB 
 
-source activate py36
+module load python/anaconda3
 module load gis/TauDEM-5.3.8-gcc-mpich
 module load gdal/2.2.2-gcc
-
+source activate py36
 
 
 python -u ./runPRISMCPG.py $SLURM_JOB_ID $SLURM_NTASKS
