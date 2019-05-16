62db40e5e7fdee245507f34bf50d35ff47982864
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-05-16 18:33:36 +0000

    Update testResample.py

---
+++ testBinary.py
@@ -0,0 +1,9 @@
+from tools import *
+
+
+inCat = "../100500010101/LandCoverMT.tif"
+
+outWorkspace = "../100500010101/work"
+
+
+cat2bin(inCat, outWorkspace)
--- testResample.py
+++
@@ -1,9 +0,0 @@
-from tools import *
-import rasterio as rs
-
-inParam = "../100500010101/2015PRISM_100500010101.tif"
-fdr = "../100500010101/fdr100500010101.tif"
-outParam = "../100500010101/PRISM100500010101.tif"
-resampleMethod = "nearest"
-
-resampleParam(inParam, fdr, outParam, resampleMethod)
