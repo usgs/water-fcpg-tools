832f919f8c8c9e7446190f6c1ba4abb5e5abcad4
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-04 19:43:00 +0000

    Add new file

---
+++ maketauDEMfiles.py
@@ -0,0 +1,18 @@
+from tools import *
+
+HUClist = ["1005", "1006", "1007", "1008", "1009", "1010", "1011", "1012"]
+
+outDir = "../data/tauDEM"
+
+cores = 16
+
+for HUC in HUClist:
+
+    fdr = "../data/NHDPLus/HRNHDPlusRasters{0}/fdr.tif".format(HUC)
+    taufdr = os.path.join(outDir, "taufdr" + HUC + ".tif")
+    taufac = os.path.join(outDir, "taufac" + HUC + ".tif")
+    
+    tauDrainDir(fdr, taufdr)
+    tauFlowAccum(taufdr, taufac, cores=cores)
+
+
