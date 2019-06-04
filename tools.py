1cd41a904438fea1a5419f5d4397f49c07c744e3
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-04 17:30:54 +0000

    Add .path

--- tools.py
+++ tools.py
@@ -612,7 +612,7 @@ def binarizeCat(val, data, nodata, outWorkspace, baseName, ext, profile):
 
 
 def downloadNHDPlusRaster(HUC4, fileDir):
-    compressedFile = os.join(fileDir, HUC4, "_RASTER.7z")
+    compressedFile = os.path.join(fileDir, HUC4, "_RASTER.7z")
     urllib.urlretrieve("https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHDPlus/HU4/HighResolution/GDB/NHDPLUS_H_%s_HU4_RASTER.7z"%str(HUC4), compressedFile)
 
     os.system( '7z x compressedFile -o fileDir')
