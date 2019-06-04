b379fb45738c24e67767e39f967998e089d8d85f
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-04 18:52:08 +0000

    Document NHDPlus download function

--- tools.py
+++ tools.py
@@ -612,6 +612,16 @@ def binarizeCat(val, data, nodata, outWorkspace, baseName, ext, profile):
 
 
 def downloadNHDPlusRaster(HUC4, fileDir):
+    '''
+    Inputs:
+        
+        HUC4 - 4 digit HUC to download NHDPlus raster data for
+        fileDir - Directory in which to save NHDPlus data
+
+    Outputs:
+        NHDPlus raster files saved to directory
+
+    '''
     compressedFile = os.path.join(fileDir, str(HUC4) + "_RASTER.7z")
     print("Downloading File: " + compressedFile)
     urllib.request.urlretrieve("https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHDPlus/HU4/HighResolution/GDB/NHDPLUS_H_%s_HU4_RASTER.7z"%str(HUC4), compressedFile)
