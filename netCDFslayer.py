4d5fbcba01cffb10566f2b36e9fe5ddee66265d4
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-06 18:56:59 +0000

    Add code to create a filename with date

--- netCDFslayer.py
+++ netCDFslayer.py
@@ -1,9 +1,12 @@
 import rasterio as rs
 import numpy as np
+import datetime
 
 # Script to destroy the netCDF file Roy got from gridMET
 
 netCDFpath = "../data/cov/gridMET_PRmm.tif"
+
+baseName = "gridMET_PRmm"
 
 with rs.open(netCDFpath) as ds: # load parameter raster
         numBands = ds.count
@@ -12,12 +15,27 @@ with rs.open(netCDFpath) as ds: # load parameter raster
         paramNoData = ds.nodata
         tags = ds.tags()
 
-dates = tags["NETCDF_DIM_time_VALUES"]
+day0 = datetime.datetime.strptime("01_01_1900", "%d_%m_%y") #Set the day time is counted from
+
+
+days = list(tags["NETCDF_DIM_time_VALUES"]) #Get the list of dates associated with each band and convert to list
+
 print(dates)
 
 i = 0 
 
 for band in data:
+
+        day = days[i] #Get the days since beginning associated with the band
+        
+        date = day0 + datetime.timedelta(days=day) #Compute the date associated with the band
+
+        fileName = baseName + "_" + .strftime('%d_%m_%Y') + ".tif" #Create the name for the output file
+
+        print(fileName)
+
+
+
 
         
 
