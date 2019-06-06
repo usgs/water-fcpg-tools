2dc744b36fb5e54dde3cec0179e92e13fc2f4e03
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-06 22:15:20 +0000

    Update tools.py

--- tools.py
+++ tools.py
@@ -332,7 +332,7 @@ def resampleParam(inParam, fdr, outParam, resampleMethod="bilinear", cores=1):
     SR = osr.SpatialReference()
     SR.ImportFromWkt(str(fdrcrs))
     fdrcrs = SR.ExportToProj4()
-    """"
+    """
     
 
     fdrcrs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
