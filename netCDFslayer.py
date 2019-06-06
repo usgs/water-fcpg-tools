3fda3c0ecaf54de08590a9ca82631d4164a39802
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-06 19:04:53 +0000

    Update netCDFslayer.py

--- netCDFslayer.py
+++ netCDFslayer.py
@@ -18,7 +18,7 @@ with rs.open(netCDFpath) as ds: # load parameter raster
 day0 = datetime.datetime.strptime("01-01-1900", "%d-%m-%Y") #Set the day time is counted from
 
 
-days = list(tags["NETCDF_DIM_time_VALUES"]) #Get the list of dates associated with each band and convert to list
+days = tags["NETCDF_DIM_time_VALUES"] #Get the list of dates associated with each band and convert to list
 
 print(days)
 
