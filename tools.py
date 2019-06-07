94324eaf4791caf0edb8525c597b8f270c28a5c9
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-07 13:20:29 +0000

    Escape "s

--- tools.py
+++ tools.py
@@ -335,7 +335,7 @@ def resampleParam(inParam, fdr, outParam, resampleMethod="bilinear", cores=1):
     """
     
 
-    fdrcrs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+    fdrcrs = "\"+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs\""
     print("Flow Direction proj4: " + str(fdrcrs))
     
     # Choose an appropriate gdal data type for the parameter
