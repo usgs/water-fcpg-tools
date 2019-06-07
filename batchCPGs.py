f1c5a9e74dfd7502f2b3104a81a31d095e9d9d18
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-07 21:25:00 +0000

    Update batchCPGs.py

--- batchCPGs.py
+++ batchCPGs.py
@@ -45,13 +45,13 @@ for cov in covList:
         f.writelines("#SBATCH --job-name=%s.job\n" %covname)
         f.writelines("#SBATCH -c 1\n") # cpus per task
         f.writelines("#SBATCH -n {0}\n".format(cores)) # number of tasks
-        #f.writelines("#SBATCH --tasks-per-node=8\n") # Set number of tasks per node
+        f.writelines("#SBATCH --tasks-per-node={0}\n".format(cores)) # Set number of tasks per node
         f.writelines("#SBATCH -p normal\n") # the partition you want to use, for this case prod is best
         f.writelines("#SBATCH --account=wymtwsc\n") # your account
         f.writelines("#SBATCH --time=00:30:00\n") # Overestimated guess at time
         f.writelines("#SBATCH --mem=128000\n") #memory in MB
         f.writelines("#SBATCH --mail-type=ALL\n") # Send email on all events
-        f.writelines("#SBATCH --mail-user=$USER@usgs.gov\n")
+        f.writelines("#SBATCH --mail-user=ssiefken@usgs.gov\n")
 
         #Set up python environment for job
         f.writelines("module load gis/TauDEM-5.3.8-gcc-mpich\n")
@@ -61,7 +61,7 @@ for cov in covList:
         #Run the python script
         f.writelines("python -u ./makeCPG.py {0} {1} {2} {3} {4} {5}\n".format(cov, taufdr, taufac, workDir, outDir, cores))
         
-    print("Launch batch job for: " + str(covname))
+    print("Launching batch job for: " + str(covname))
 
     os.system("sbatch {0}".format(jobfile)) #Send command to console
 
