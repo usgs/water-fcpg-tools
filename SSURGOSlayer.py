df97cb0717774ced17b1cb90068fa3b903cd86fd
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-14 21:36:15 +0000

    Update SSURGOSlayer.py

--- SSURGOSlayer.py
+++ SSURGOSlayer.py
@@ -27,7 +27,7 @@ for paramtable in paramtables:
     df = pd.read_csv(csvName)
 
     print(list(df))
-    d = pd.concat(df["(b'mukey', 5)"], df["(b'drnclass_1', 10)"])
+    d = pd.concat([df["(b'mukey', 5)"], df["(b'drnclass_1', 10)"]], axis=1)
 
     print(d)
 
