baa41bc406e1af61dfc6f7378e3ce0e77a6e9c51
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-14 20:53:39 +0000

    Update SSURGOSlayer.py

--- SSURGOSlayer.py
+++ SSURGOSlayer.py
@@ -5,11 +5,23 @@ import numpy as np
 
 paramtables = ["../data/cov/static/Soils/DrainageClass.dbf"]
 
+outDir = "../data/cov/static/Soils/work"
+
 #Convert dbfs to csvs
 for paramtable in paramtables:
 
     db = dbf.Table(paramtable)
     dbf.export(db)
+
+    paramName = os.path.splitext(os.path.basename(paramtable))[0]
+    print(paramName)
+
+    csvName = os.path.join(outDir, paramName + ".csv") #Create csv file
+
+    db = dbf.Table('temptable.dbf')
+    db.open()
+
+    dbf.export(db, csvName, header = True)
 
     #df = pd.read_csv()
 
