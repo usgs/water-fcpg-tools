f446f1ead657c61315369268bc52ccfd46f6fa69
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-14 20:30:58 +0000

    Update SSURGOSlayer.py

--- SSURGOSlayer.py
+++ SSURGOSlayer.py
@@ -1,5 +1,10 @@
 import dbf
 import os
 
+paramtables = ["../data/cov/static/Soils/DrainageClass.dbf"]
 
+#Convert dbfs to csvs
+for paramtable in paramtables:
 
+    db = dbf.Table(paramtable)
+    dbf.export(db)
