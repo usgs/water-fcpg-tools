15f84c4c0e5c94d47d54002eca9abef625030c35
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-21 19:02:09 +0000

    Create file names for each HUC

--- batchHUCs.py
+++ batchHUCs.py
@@ -2,22 +2,22 @@ import os
 
 HUClist = []
 
-    inDir = "../data/cov/gridMET_SOILMOISTmm" 
-    taufdr = "../data/tauDEM" 
-    taufac = "../data/tauDEM" 
-    workDir = "../work/"
-    outDir = "../CPGs/"
-    logDir = "../logs/"
-    cores = 20
-    accumThresh = 1000
-    overwrite = True
+
+inDir = "../data/cov/gridMET_SOILMOISTmm" 
+taufdr = "../data/tauDEM" 
+taufac = "../data/tauDEM" 
+workDir = "../work"
+outDir = "../CPGs"
+logDir = "../logs"
+cores = 20
+accumThresh = 1000
+overwrite = True
+
 
 for HUC in HUClist:
 
-    fdr = "../data/NHDPlus05_06_2019/HRNHDPlusRasters{0}/fdr.tif".format(HUC)
-    taufdr = os.path.join(outDir, "taufdr" + HUC + ".tif")
-    taufac = os.path.join(outDir, "taufac" + HUC + ".tif")
-    
-    tauDrainDir(fdr, taufdr)
-    #tauFlowAccum(taufdr, taufac, cores=cores)
-
+    HUCtaufdr = os.path.join(taufdr, "taufdr{0}.tif".format(HUC))
+    HUCtaufac = os.path.join(taufac, "taufac{0}.tif".format(HUC))
+    HUCworkDir = os.path.join(workDir, HUC)
+    HUCoutDir = os.path.join(outDir, HUC)
+    HUClogDir = os.path.join(logDir, HUC)
