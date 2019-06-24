3ae801eb5bcaa164ddc797d566d1d9ca4899ee80
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-24 16:49:11 +0000

    Update netCDFTempslayer.py

--- netCDFTempslayer.py
+++ netCDFTempslayer.py
@@ -94,7 +94,7 @@ profile.update({
 for month in monthlyData:
         fileName = os.path.join(outDir, "{0}_{1}_{2}_00.tif".format(baseName, year, month)) #Create the name for the output file
 
-        monthlyMin = np.minimum.reduce(monthlyData[month]) #Compute minimum temp in each cell for the month
+        monthlyMin = np.meane(monthlyData[month], axis=0) #Compute average minimum temp in each cell for the month
 
         """
         with rs.open(fileName, 'w', **profile) as dst:
