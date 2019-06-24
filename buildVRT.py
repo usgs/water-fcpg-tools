32c6f1c97a1982ee30ad46df5f64fab83a28dd67
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-24 22:15:20 +0000

    Set up script to create VRTs

---
+++ buildVRT.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+import traceback
+
+
+
+rasterList = [] #Initialize list of covariates
+
+for path, subdirs, files in os.walk(inDir):
+    for name in files:
+        #Check if file is .tif, and if so add it to covariate list
+        if os.path.splitext(name)[1] == ".tif":
+                rasterList.append(os.path.join(path, name))
+
+
+
+
+years = range(1985, 2019)
+
+
+for year in years:
+
+    rasterList = []
+
+    try:
+        print('Accumulating No Data Values')
+
+
+        cmd = 'gddalbuildvrt -bind-to rr -n {cores} aread8 -p {fdr} -ad8 {outFl} -wg {weight} -nc'.format(**tauParams) # Create string of tauDEM shell command
+        print(cmd)
+        result = subprocess.run(cmd, shell = True) # Run shell command
+        result.stdout
+        print("Parameter no data accumulation written to: {0}".format(outNoDataRast))
+
+        except:
+        print('Error Accumulating Data')
+        traceback.print_exc()
