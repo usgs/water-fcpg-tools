aa967720e9eb4e761c5368b4def5405d39ef4f7c
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-24 20:29:50 +0000

    Update netCDFTempslayer.py

--- netCDFTempslayer.py
+++ netCDFTempslayer.py
@@ -8,9 +8,9 @@ import traceback
 # Script to destroy the netCDF file Roy got from gridMET
 # Must have gdal and nco tools (module load tools/nco-4.7.8-gnu) modules loaded 
 
-inCDF = "../data/cov/minTempNCDF/tmmn_1996.nc" #Original netCDF from gridMET
-reorderCDF = "../data/cov/minTempNCDF/tmmn_1996fix.nc" #NetCDF file with reordered dimensions
-multiTIFF = "../data/cov/minTempNCDF/tmmn_1996.tif" #Multiband .tif created from netCDF
+inCDF = "../data/cov/minTempNCDF/tmmn_1997.nc" #Original netCDF from gridMET
+reorderCDF = "../data/cov/minTempNCDF/tmmn_1997fix.nc" #NetCDF file with reordered dimensions
+multiTIFF = "../data/cov/minTempNCDF/tmmn_1997.tif" #Multiband .tif created from netCDF
 
 baseName = "gridMET_minTempK"
 
