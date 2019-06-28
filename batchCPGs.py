299c4bb9d3fe74a12a34669ec10a798d0a300d41
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-28 13:15:49 +0000

    Run HUC 1013 min Temp

--- batchCPGs.py
+++ batchCPGs.py
@@ -15,7 +15,7 @@ if len(sys.argv) > 1:
     deleteTemp = sys.argv[10] #Whether to delete temporary files
 else:
     #If inputs aren't specified in system args, set them in the script
-    inDir = "../data/cov/gridMET_SOILMOISTmm/gridMET_SOILMOISTmm_1987_03_31.tif" 
+    inDir = "../data/cov/gridMET_minTempK" 
     taufdr = "../data/tauDEM/taufdr1013.tif" 
     taufac = "../data/tauDEM/taufac1013.tif" 
     workDir = "../work/1013"
