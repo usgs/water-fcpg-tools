20ef07a76ce08e90728529e3f64a23b0221854e7
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-06-28 17:13:58 +0000

    Update submitTest.sh

--- submitTest.sh
+++ submitTest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=CPG # name that you chose
 #SBATCH -c 1 # cpus per task
-#SBATCH -n 16 # number of tasks
-#SBATCH --tasks-per-node=16
+#SBATCH -n 20 # number of tasks
+#SBATCH --tasks-per-node=20
 #SBATCH -p normal                         # the partition you want to use, for this case prod is best
 #SBATCH --account=wymtwsc        # your account
-#SBATCH --time=01:00:00           # Overestimated guess at time
+#SBATCH --time=02:00:00           # Overestimated guess at time
 #SBATCH --mail-type=ALL         # Send email on all events
 #SBATCH --mail-user=ssiefken@usgs.gov
 #SBATCH -o %j.log                    # Sets output log file to %j ( will be the jobId returned by sbatch)  
@@ -15,4 +15,4 @@ source activate py36
 module load gis/TauDEM-5.3.8-gcc-mpich
 module load gdal/2.2.2-gcc
 
-python -u ./SSURGOSlayer.py
+python -u ./makeDinfinity.py
