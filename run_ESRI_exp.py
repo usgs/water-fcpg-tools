cb8f2fd42e3d36b00acd7fb856273c48d5fc1cf7
Theodore Barnhart <tbarnhart@usgs.gov>
2019-06-28 17:18:56 -0600

    got multiple delineations and zonal stats working in arcpy, yay

--- run_ESRI_exp.py
+++ run_ESRI_exp.py
@@ -1,62 +1,89 @@
+#from builtins import range
 import time
-strt = time.time()
-
 import arcpy
 arcpy.CheckOutExtension("spatial")
 from arcpy.sa import *
 import os
 import sys
 
-numSamples = sys.argv[1] # number of samples to run 
-runNum = sys.argv[2] # run number
-samples = sys.argv[3] # samples to run
-outfl = sys.argv[4] # output file location
+def delineate(numIter):
+	loadStrt = time.time()
+	numIter = int(numIter)
 
-arcpy.env.workspace = r"C:/Users/tbarnhart/projects/CPGtools/data/CHI_poster"
+	#numIter = int(sys.argv[1]) # number of samples to run 
+	#outfl = sys.argv[2] # output file location
 
-fdrPath = 'HRNHDPlusRasters1003/fdr.tif'
-paramPath = 'daymet_v3_prcp_annttl_2012_na.tif'
+	arcpy.env.workspace = "C:\\Users\\tbarnhart\\projects\\CPGtools\\data\\CHI_poster"
 
-arcpy.env.extent = fdrPath
-arcpy.env.snapRaster = fdrPath
-arcpy.env.cellSize = fdrPath
-arcpy.env.overwriteOutput = True 
+	fdrPath = os.path.join(arcpy.env.workspace,'HRNHDPlusRasters1003\\fdr.tif')
+	paramPath = os.path.join(arcpy.env.workspace,'daymet\\daymet_v3_prcp_annttl_2017_na_reproj.tif')
 
-sampLyr = "sampLyr"
-arcpy.MakeFeatureLayer_management(samples, sampLyr)
-#samp = arcpy.SelectLayerByAttribute_management(sampLyr, "NEW_SELECTION", '"idx" <= %i'%(numSamples))
+	arcpy.env.extent = fdrPath
+	arcpy.env.snapRaster = fdrPath
+	arcpy.env.cellSize = fdrPath
+	arcpy.env.overwriteOutput = True 
 
-fdr = Raster(fdrPath) # load the FDR
-param = Raster(paramPath) # load the parameter
+	fdr = Raster(fdrPath) # load the FDR
+	param = Raster(paramPath) # load the parameter
 
-arcpy.AddMessage("Delineating watersheds for run: %s, %s points."%(runNum, numSamples))
+	strt = time.time()
+	arcpy.AddMessage("Run %s"%(numIter))
 
-delineation = Con(IsNull(fdr) == 0,0,'')
-with arcpy.da.SearchCursor(samples,['idx']) as cursor: # loop through each point
-	for field in cursor:
-		samp = arcpy.SelectLayerByAttribute_management(sampLyr, "NEW_SELECTION", '"idx" = %i'%field[0]) # select each point
-		delineation += Watershed(fdr,samp,"idx") # run the watershed and sum onto output raster
-		arcpy.SelectLayerByAttribute_management(samp, "CLEAR_SELECTION") # start over
+	outFeat = 'watersheds_%i.shp'%(numIter) # path to the output shapefile
+	samples = 'sample_locs_%i.shp'%(numIter)
 
-delineation = SetNull(IsNull(fdr),delineation)
+	delineationPath = "ESRI_delineation_%i.tif"%(int(numIter)) # path tou output raster
 
-delineation.save("ESRI_delineation_%i.tif"%(int(runNum)))
+	if arcpy.Exists("ESRI_res_%i.dbf"%(numIter)):
+		arcpy.Delete_management("ESRI_res_%i.dbf"%(numIter))
 
-arcpy.AddMessage("Computing Zonal Stats for run: %s, %s watersheds."%(runNum, numSamples))
-outStats = ZonalStatisticsAsTable(delineation,"Value", param,"ESRI_res_%i.dbf"%int(runNum),"DATA","MEAN")
+	if arcpy.Exists(outFeat):
+		arcpy.Delete_management(outFeat)
 
+	if arcpy.Exists(delineationPath):
+		arcpy.Delete_management(delineationPath)
 
-totalTime = time.time() - strt
-arcpy.AddMessage("run time: %s minutes."%(totalTime/60.))
+	sampLyr = 'sampLyr'
+	PP = 'pp.tif'
+	arcpy.MakeFeatureLayer_management(samples,sampLyr)
 
-outfl = os.path.join(arcpy.env.workspace,outfl)
+	loadTotal = time.time() - loadStrt
 
-arcpy.AddMessage("Writing Output to : %s"%outfl)
-if not os.path.isfile(outfl):
-	with open(outfl,'w') as dst:
-		dst.write('RunNum,Time\n')
-		dst.write('%s,%s\n'%(runNum,totalTime))
-else:
-	with open(outfl,'a') as dst:
-		dst.write('%s,%s\n'%(runNum,totalTime))
-arcpy.AddMessage("Output Complete.")
+	tmp = Int(Con(IsNull(fdr)==0,0)) # make empty raster
+	wshds = []
+	with arcpy.da.SearchCursor(samples,['idx']) as cursor: # iterate through each point
+		for field in cursor:
+			arcpy.AddMessage('	Delineating watershed %i'%field[0])
+			arcpy.SelectLayerByAttribute_management(sampLyr,"NEW_SELECTION",'"idx" = %s'%field[0])
+
+			arcpy.FeatureToRaster_conversion(sampLyr,"idx",PP,10)
+
+			wshd = Watershed(fdr, PP) # compute watershed
+			tmp += Int(Con(IsNull(wshd)==0,int(field[0]),0)) # add the idx number to the delineation raster, before, this had nulls, wich was generating an all-null raster
+			#wshds.append(Int(Con(IsNull(wshd)==0,int(field[0]),0)))
+			arcpy.SelectLayerByAttribute_management(sampLyr,"CLEAR_SELECTION")
+
+	#arcpy.MosaicToNewRaster_management(wshds,arcp.env.workspace, delineationPath, pixel_type="32_BIT_UNSIGNED", mosaic_method = "SUM")
+	#delineation = Raster(delineationPath)
+	delineation = Int(Con((IsNull(fdr)==0) & (tmp != 0),tmp,'')) # mask out the no data areas again.
+	delineation.save(delineationPath)
+
+	arcpy.AddMessage("	Computing Zonal Stats.")
+	outStats = ZonalStatisticsAsTable(delineation,"Value", param,"ESRI_res_%i.dbf"%(numIter),"DATA","MEAN")
+
+	runTime = time.time() - strt
+	arcpy.AddMessage("	Run time: %s minutes."%(runTime/60.))
+
+	#outfl = os.path.join(arcpy.env.workspace,outfl)
+
+	# arcpy.AddMessage("	Writing Output to : %s"%outfl)
+	# if not os.path.isfile(outfl):
+	# 	with open(outfl,'w') as dst:
+	# 		dst.write('RunNum,Time\n')
+	# 		dst.write('%s,%s\n'%(numIter,totalTime))
+	# else:
+	# 	with open(outfl,'a') as dst:
+	# 		dst.write('%s,%s\n'%(numIter,totalTime))
+	# arcpy.AddMessage("	Output Complete.")
+
+	return loadTotal, runTime
