94a461742768041345125db8a052b159f1acc477
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-07-08 15:26:30 +0000

    Initial script

---
+++ decayAccum.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+import rasterio as rs
+
+
+
+def makeDecayGrid(fdr, multiplier, outRast):
+
+    if not os.path.isfile(fdr):
+        print("Error - Flow direction raster file is missing!")
+        return #Function will fail, so end it now
+
+
+    with rs.open(fdr) as ds: # load flow direction data
+        data = ds.read(1)
+        profile = ds.profile
+        inNoData = ds.nodata
+
+    decayGrid = data.astype(np.float32) #Convert to float
+    decayGrid[data != inNoData] = multiplier # fill all data cells with with multiplier values
+
+
+    # Update raster profile
+    profile.update({
+                'dtype': decayGrid.dtype,
+                'compress':'LZW',
+                'profile':'GeoTIFF',
+                'tiled':True,
+                'sparse_ok':True,
+                'num_threads':'ALL_CPUS',
+                'nodata':inNoData,
+                'bigtiff':'IF_SAFER'})
+
+    print("Saving decay raster {0}".format(datetime.datetime.now()))
+    
+    with rs.open(outRast, 'w', **profile) as dst:
+        dst.write(decayGrid,1)
+        print("Decay raster written to: {0}".format(outRast))
+
+        
+
+    
+
