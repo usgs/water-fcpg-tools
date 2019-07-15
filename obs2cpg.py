9b82e6e30c758ffe7b3e4c66fccb6ec53f83f3c4
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-07-15 21:26:31 +0000

    Update obs2cpg.py

--- obs2cpg.py
+++ obs2cpg.py
@@ -14,9 +14,9 @@ data = pd.read_csv(obsFile)
 data.Date =  pd.to_datetime(data.Date, format='%m/%d/%Y %H:%M:%S')
 
 #Populate the year, month, and day columns
-data.Year = pd.DatetimeIndex(data.Date).year
-data.Month = pd.DatetimeIndex(data.Date).month
-data.Day = pd.DatetimeIndex(data.Date).day
+data.Year = pd.to_datetime(data.Date, format='%Y')
+data.Month = pd.DatetimeIndex(data.Date, format='%m')
+data.Day = pd.DatetimeIndex(data.Date, format='%d')
 
 #Create column with tuple of point coordinates
 data['USGS_Albers'] = list(zip(data.POINTX, data.POINTY))
