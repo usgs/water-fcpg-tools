38cce0dc71adfd5cc066a3546abeaefb67fa5d0e
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-07-15 14:16:19 +0000

    Add function to mask streams

--- tools.py
+++ tools.py
@@ -504,7 +504,57 @@ def dist2stream(fdr, fac, thresh, outRast, cores=1) :
         print('Error computing distances')
         traceback.print_exc()
 
+def maskStreams(inRast, fac, outRast, minAccum = None):
+    '''
+    Inputs:
+        
+        inRast - input raster to mask
+        fac - flow accumulation raster
+        outRast - output raster file location
+        minAccum - value of flow accumulation below which the raster values will be set to no data
+        
 
+    Outputs:
+        outRast - raster with cells below minimum accumulation set to no data
+    '''
+  
+    
+    if not os.path.isfile(inRast):
+        print("Error - Parameter raster file is missing!")
+        return #Function will fail, so end it now
+    if not os.path.isfile(fac):
+        print("Error - Flow accumulation file is missing!")
+        return #Function will fail, so end it now
+
+
+    with rs.open(inRast) as ds: # load input raster
+        data = ds.read(1)
+        profile = ds.profile
+        inNoData = ds.nodata 
+
+    with rs.open(fac) as ds: # load input raster
+    accum = ds.read(1)
+
+
+    # Replace values in cells with small flow accumulation with no data
+    if minAccum != None:
+        print("Replacing small flow accumulations {0}".format(datetime.datetime.now()))
+        data[accum < minAccum] = inNoData #Set values smaller than threshold to no data
+
+    # Update raster profile
+    profile.update({
+                'compress':'LZW',
+                'profile':'GeoTIFF',
+                'tiled':True,
+                'sparse_ok':True,
+                'num_threads':'ALL_CPUS',
+                'nodata':inNoData,
+                'bigtiff':'IF_SAFER'})
+
+    print("Saving raster {0}".format(datetime.datetime.now()))
+    with rs.open(outRast, 'w', **profile) as dst:
+        dst.write(data,1)
+        print("CPG file written to: {0}".format(outRast))
 
 
 
