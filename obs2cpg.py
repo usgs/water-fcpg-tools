4bc9367b70e4a202093d8ff74c21fe85c044584f
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-07-15 21:31:23 +0000

    Update obs2cpg.py

--- obs2cpg.py
+++ obs2cpg.py
@@ -14,7 +14,7 @@ data = pd.read_csv(obsFile)
 data.Date =  pd.to_datetime(data.Date, format='%m/%d/%Y %H:%M:%S')
 
 #Populate the year, month, and day columns
-data.Year = pd.to_datetime(data.Date, format='%Y')
+data.Year = pd.to_datetime(data.Date, format='%Y/%m/%d')
 data.Month = pd.to_datetime(data.Date, format='%m')
 data.Day = pd.to_datetime(data.Date, format='%d')
 
