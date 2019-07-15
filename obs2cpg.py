42fbe8123407a789e5304008225e2316999d5ce6
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-07-15 20:09:07 +0000

    Update obs2cpg.py

--- obs2cpg.py
+++ obs2cpg.py
@@ -74,7 +74,7 @@ for param in static:
 
     paramCPG = os.path.join(CPGdir, "{0}_HUC{1}_CPG.tif".format(param, HUC)) #Build path to CPG file
 
-    with rs.open(param) as ds:
+    with rs.open(paramCPG) as ds:
         CPGvalues = ds.sample(data['USGS_Albers'])
         print(CPGvalues)
 
