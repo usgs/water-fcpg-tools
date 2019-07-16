064f948d38009facb6c726caf0e092663a3b9503
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-07-16 16:09:32 +0000

    Add dynamic parameter lists

--- obs2cpg.py
+++ obs2cpg.py
@@ -80,6 +80,18 @@ for param in static:
         CPGvalues = ds.sample([(-124542,44226)],1)
 
 
+dynamicPaths = data[['FID', 'Lat', 'Long', 'Date', 'Year', 'Month', 'Day']].copy() #Create dataframe to store file paths to dynamic CPGs
+
+
+#Dynamic parameter lists
+
+gridMET_PRmmList = [gridMET_PRmm_Oct, gridMET_PRmm_Nov, gridMET_PRmm_Dec, gridMET_PRmm_Jan, gridMET_PRmm_Feb, gridMET_PRmm_Mar, gridMET_PRmm_Apr, gridMET_PRmm_May, gridMET_PRmm_Jun, gridMET_PRmm_Jul, gridMET_PRmm_Aug, gridMET_PRmm_Sep]
+gridMET_minTempKList = [gridMET_minTempK_Oct, gridMET_minTempK_Nov, gridMET_minTempK_Dec, gridMET_minTempK_Jan, gridMET_minTempK_Feb, gridMET_minTempK_Mar, gridMET_minTempK_Apr, gridMET_minTempK_May, gridMET_minTempK_Jun, gridMET_minTempK_Jul, gridMET_minTempK_Aug, gridMET_minTempK_Sep]
+gridMET_SOILMOISTmmList = [gridMET_SOILMOISTmm_Oct, gridMET_SOILMOISTmm_Nov, gridMET_SOILMOISTmm_Dec, gridMET_SOILMOISTmm_Jan, gridMET_SOILMOISTmm_Feb, gridMET_SOILMOISTmm_Mar, gridMET_SOILMOISTmm_Apr, gridMET_SOILMOISTmm_May, gridMET_SOILMOISTmm_Jun, gridMET_SOILMOISTmm_Jul, gridMET_SOILMOISTmm_Aug, gridMET_SOILMOISTmm_Sep]
+
+SNODAS_SWEmmList = [SNODAS_SWEmm_Oct, SNODAS_SWEmm_Nov, SNODAS_SWEmm_Dec, SNODAS_SWEmm_Jan, SNODAS_SWEmm_Feb, SNODAS_SWEmm_Mar, SNODAS_SWEmm_Apr, SNODAS_SWEmm_May, SNODAS_SWEmm_Jun, SNODAS_SWEmm_Jul, SNODAS_SWEmm_Aug, SNODAS_SWEmm_Sep]
+
+
 
 for index, row in data.iterrows():
 
