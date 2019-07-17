3f4a358c98e98e9462c30104627ad8369ebccc1b
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-07-17 14:37:02 +0000

    Update obs2cpg.py

--- obs2cpg.py
+++ obs2cpg.py
@@ -11,114 +11,9 @@ obsFile = "../data/observations/flowPerm_07152019_USGSAlbers_HUC1002.csv"
 CPGdir = "../CPGs/1002"
 HUC = 1002
 
-#Load observations file to dataframe
-data = pd.read_csv(obsFile, encoding = "ISO-8859-1")
-
-data.Date =  pd.to_datetime(data.Date, format='%m/%d/%Y')
-
-#Populate the year, month, and day columns
-data.Year = pd.DatetimeIndex(data.Date).year.map("{:04}".format)
-data.Month = pd.DatetimeIndex(data.Date).month.map("{:02}".format)
-data.Day = pd.DatetimeIndex(data.Date).day.map("{:02}".format)
-
-#Create column with tuple of point coordinates
-data['USGS_Albers'] = list(zip(data.POINTX, data.POINTY))
 
 
-#Get lists of static and dynamic CPGs
-
-CPGs = [] #Initialize list of CPGs
-
-
-if os.path.isdir(CPGdir):
-    #Get all covariate files in directory
-    for path, subdirs, files in os.walk(CPGdir):
-        for name in files:
-            #Check if file is .tif, and if so add it to covariate list
-            if os.path.splitext(name)[1] == ".tif":
-                    CPGs.append(os.path.join(path, name))
-else:
-    print("Invalid CPG directory")
-
-#Get unique CPG parameters
-
-def getCPGname(CPG):
-    basename = os.path.splitext(os.path.basename(CPG))[0] #Get name of CPG file without extention
-    paramname = basename.split("_")[0] + "_" + basename.split("_")[1] #Get name of CPG parameter
-    return paramname
-
-paramNames = list(map(getCPGname, CPGs))
-
-paramNum = Counter(paramNames) #Counter number of times each parameter name occurs
-
-#Create empty sets for dynamic and static parameter lists
-dynamic = set()
-static = set()
-
-for param in set(paramNames):
-    if paramNum[param] > 1:
-        dynamic.add(param)
-    else:
-        static.add(param)
- 
-#Sort the parameter paramNames
-dynamic = list(dynamic)
-static = list(static)
-dynamic.sort()
-static.sort()
-
-
-#Prepare intermediate data frames
-staticValues = pd.concat([data, pd.DataFrame(columns=static)], sort=False)
-
-
-dynamicPaths = data[['FID', 'Lat', 'Long', 'Site_ID', 'Waterbody', 'Date', 'Year', 'Month', 'Day', 'USGS_Albers', 'Source', 'Binary']].copy() #Create dataframe to store file paths to dynamic CPGs
-
-
-#Dynamic parameter lists
-gridMET_PRmmList = ["gridMET_PRmm_Oct", "gridMET_PRmm_Nov", "gridMET_PRmm_Dec", "gridMET_PRmm_Jan", "gridMET_PRmm_Feb", "gridMET_PRmm_Mar", "gridMET_PRmm_Apr", "gridMET_PRmm_May", "gridMET_PRmm_Jun", "gridMET_PRmm_Jul", "gridMET_PRmm_Aug", "gridMET_PRmm_Sep"]
-gridMET_minTempKList = ["gridMET_minTempK_Oct", "gridMET_minTempK_Nov", "gridMET_minTempK_Dec", "gridMET_minTempK_Jan", "gridMET_minTempK_Feb", "gridMET_minTempK_Mar", "gridMET_minTempK_Apr", "gridMET_minTempK_May", "gridMET_minTempK_Jun", "gridMET_minTempK_Jul", "gridMET_minTempK_Aug", "gridMET_minTempK_Sep"]
-gridMET_SOILMOISTmmList = ["gridMET_SOILMOISTmm_Oct", "gridMET_SOILMOISTmm_Nov", "gridMET_SOILMOISTmm_Dec", "gridMET_SOILMOISTmm_Jan", "gridMET_SOILMOISTmm_Feb", "gridMET_SOILMOISTmm_Mar", "gridMET_SOILMOISTmm_Apr", "gridMET_SOILMOISTmm_May", "gridMET_SOILMOISTmm_Jun", "gridMET_SOILMOISTmm_Jul", "gridMET_SOILMOISTmm_Aug", "gridMET_SOILMOISTmm_Sep"]
-
-SNODAS_SWEmmList = [ "SNODAS_SWEmm_Jan", "SNODAS_SWEmm_Feb", "SNODAS_SWEmm_Mar", "SNODAS_SWEmm_Apr", "SNODAS_SWEmm_May", "SNODAS_SWEmm_Jun", "SNODAS_SWEmm_Jul"]
-
-landsat_NDVIMayOctList = ["landsat_NDVI-May-Oct"]
-
-dynamicPaths = pd.concat([dynamicPaths, pd.DataFrame(columns=gridMET_SOILMOISTmmList), pd.DataFrame(columns=SNODAS_SWEmmList), pd.DataFrame(columns=landsat_NDVIMayOctList), pd.DataFrame(columns=gridMET_minTempKList)], sort=False)
-
-dynamicValues = dynamicPaths.copy()
-
-
-#Create dataframe to store all needed parameter values
-paramValues = pd.concat([data, pd.DataFrame(columns=static), pd.DataFrame(columns=gridMET_SOILMOISTmmList), pd.DataFrame(columns=SNODAS_SWEmmList), pd.DataFrame(columns=landsat_NDVIMayOctList), pd.DataFrame(columns=gridMET_minTempKList)], sort=False)
-
-
-
-#Get static CPG values
-
-points = data.USGS_Albers #Get list of data points
-print(points)
-
-for param in static:
-
-    paramCPG = os.path.join(CPGdir, "{0}_HUC{1}_CPG.tif".format(param, HUC)) #Build path to CPG file
-    print(paramCPG)
-    with rs.open(paramCPG) as ds:
-        #CPGvalues = ds.sample(list(data['USGS_Albers']),1)
-        CPGvalues = ds.sample(points)
-
-        for index, row in paramValues.iterrows():
-            paramValues.at[index, param]= next(CPGvalues)[0]
-            
-        
-
-print(paramValues)
-
-
-
-
-
-
+""" BEGIN PARAMTER FUNCTIONS """"
 
 def SNODAS_SWEmm_fcn(HUC, year, month):
 
@@ -196,6 +91,116 @@ def gridMET_minTempK_fcn(HUC, year, month):
 
 
 
+""" END PARAMETER FUNCTIONS """
+
+
+
+
+
+
+
+
+#Load observations file to dataframe
+data = pd.read_csv(obsFile, encoding = "ISO-8859-1")
+
+#Convert date to pandas datetime
+data.Date =  pd.to_datetime(data.Date, format='%m/%d/%Y')
+
+#Populate the year, month, and day columns
+data.Year = pd.DatetimeIndex(data.Date).year.map("{:04}".format)
+data.Month = pd.DatetimeIndex(data.Date).month.map("{:02}".format)
+data.Day = pd.DatetimeIndex(data.Date).day.map("{:02}".format)
+
+#Create column with tuple of point coordinates
+data['USGS_Albers'] = list(zip(data.POINTX, data.POINTY))
+
+
+#Get lists of static and dynamic CPGs
+
+CPGs = [] #Initialize list of CPGs
+
+
+if os.path.isdir(CPGdir):
+    #Get all covariate files in directory
+    for path, subdirs, files in os.walk(CPGdir):
+        for name in files:
+            #Check if file is .tif, and if so add it to covariate list
+            if os.path.splitext(name)[1] == ".tif":
+                    CPGs.append(os.path.join(path, name))
+else:
+    print("Invalid CPG directory")
+
+#Get unique CPG parameters
+
+def getCPGname(CPG):
+    basename = os.path.splitext(os.path.basename(CPG))[0] #Get name of CPG file without extention
+    paramname = basename.split("_")[0] + "_" + basename.split("_")[1] #Get name of CPG parameter
+    return paramname
+
+paramNames = list(map(getCPGname, CPGs))
+
+paramNum = Counter(paramNames) #Counter number of times each parameter name occurs
+
+#Create empty sets for dynamic and static parameter lists
+dynamic = set()
+static = set()
+
+for param in set(paramNames):
+    if paramNum[param] > 1:
+        dynamic.add(param)
+    else:
+        static.add(param)
+ 
+#Sort the parameter paramNames
+dynamic = list(dynamic)
+static = list(static)
+dynamic.sort()
+static.sort()
+
+
+#Prepare intermediate data frames
+staticValues = pd.concat([data, pd.DataFrame(columns=static)], sort=False)
+
+
+#Dynamic parameter lists
+gridMET_PRmmList = ["gridMET_PRmm_Oct", "gridMET_PRmm_Nov", "gridMET_PRmm_Dec", "gridMET_PRmm_Jan", "gridMET_PRmm_Feb", "gridMET_PRmm_Mar", "gridMET_PRmm_Apr", "gridMET_PRmm_May", "gridMET_PRmm_Jun", "gridMET_PRmm_Jul", "gridMET_PRmm_Aug", "gridMET_PRmm_Sep"]
+gridMET_minTempKList = ["gridMET_minTempK_Oct", "gridMET_minTempK_Nov", "gridMET_minTempK_Dec", "gridMET_minTempK_Jan", "gridMET_minTempK_Feb", "gridMET_minTempK_Mar", "gridMET_minTempK_Apr", "gridMET_minTempK_May", "gridMET_minTempK_Jun", "gridMET_minTempK_Jul", "gridMET_minTempK_Aug", "gridMET_minTempK_Sep"]
+gridMET_SOILMOISTmmList = ["gridMET_SOILMOISTmm_Oct", "gridMET_SOILMOISTmm_Nov", "gridMET_SOILMOISTmm_Dec", "gridMET_SOILMOISTmm_Jan", "gridMET_SOILMOISTmm_Feb", "gridMET_SOILMOISTmm_Mar", "gridMET_SOILMOISTmm_Apr", "gridMET_SOILMOISTmm_May", "gridMET_SOILMOISTmm_Jun", "gridMET_SOILMOISTmm_Jul", "gridMET_SOILMOISTmm_Aug", "gridMET_SOILMOISTmm_Sep"]
+
+SNODAS_SWEmmList = [ "SNODAS_SWEmm_Jan", "SNODAS_SWEmm_Feb", "SNODAS_SWEmm_Mar", "SNODAS_SWEmm_Apr", "SNODAS_SWEmm_May", "SNODAS_SWEmm_Jun", "SNODAS_SWEmm_Jul"]
+
+landsat_NDVIMayOctList = ["landsat_NDVI-May-Oct"]
+
+dynamicPaths = pd.concat([data, pd.DataFrame(columns=gridMET_SOILMOISTmmList), pd.DataFrame(columns=SNODAS_SWEmmList), pd.DataFrame(columns=landsat_NDVIMayOctList), pd.DataFrame(columns=gridMET_minTempKList)], sort=False) #Create dataframe to store file paths to dynamic CPGs
+
+
+
+
+#Create dataframe to store all needed parameter values
+paramValues = pd.concat([data, pd.DataFrame(columns=static), pd.DataFrame(columns=gridMET_SOILMOISTmmList), pd.DataFrame(columns=SNODAS_SWEmmList), pd.DataFrame(columns=landsat_NDVIMayOctList), pd.DataFrame(columns=gridMET_minTempKList)], sort=False)
+
+
+
+#Get static CPG values
+points = data.USGS_Albers #Get list of data points
+
+for param in static:
+
+    paramCPG = os.path.join(CPGdir, "{0}_HUC{1}_CPG.tif".format(param, HUC)) #Build path to CPG file
+    print(paramCPG)
+    with rs.open(paramCPG) as ds:
+        CPGvalues = ds.sample(points) #Read the parameter CPG at all data points
+
+        #Add CPG values to dataframe
+        for index, row in paramValues.iterrows():
+            paramValues.at[index, param]= next(CPGvalues)[0]
+            
+
+
+
+
+#Get dynamic CPG values
+
 monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 dynamicParams = [("SNODAS_SWEmm", SNODAS_SWEmm_fcn), ("gridMET_minTempK", gridMET_minTempK_fcn)]
@@ -214,9 +219,9 @@ for index, row in dynamicPaths.iterrows():
         
         for key, value in CPGdict.items():
             
-            dynamicPaths.at[index, "{0}_{1}".format(paramName, key)]= value #Add the file path to the data frame
+            #dynamicPaths.at[index, "{0}_{1}".format(paramName, key)]= value #Add the file path to the data frame
 
-            print("{0}_{1}".format(paramName, key))
+            #print("{0}_{1}".format(paramName, key))
 
             paramCPG = value
             coords = row['USGS_Albers']
@@ -224,8 +229,8 @@ for index, row in dynamicPaths.iterrows():
             if os.path.isfile(paramCPG):
                 with rs.open(paramCPG) as ds:
 
-                    #CPGvalues = ds.sample(list(data['USGS_Albers']),1)
                     CPGvalues = ds.sample([coords],1)
+                    
                     try:
                         CPGval = next(CPGvalues)
                         paramValues.at[index, "{0}_{1}".format(paramName, key)]= CPGval[0]
