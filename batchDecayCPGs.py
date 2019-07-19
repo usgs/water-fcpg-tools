214f1ef33c3544ff77774080ba1d4d4d384b6c23
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-07-19 14:57:03 +0000

    Run HUC 1005

--- batchDecayCPGs.py
+++ batchDecayCPGs.py
@@ -17,12 +17,12 @@ if len(sys.argv) > 1:
 else:
     #If inputs aren't specified in system args, set them in the script
     inDir = "../data/cov/NDVI_eulersZ/vrt" 
-    tauDINFang = "../data/tauDEM/tauDINFang1004.tif" 
-    taufac = "../data/tauDEM/taufac1004.tif" 
-    invDist = "../data/tauDEM/invDist1004.tif" 
-    workDir = "../work/1004"
-    outDir = "../CPGs/1004"
-    logDir = "../logs/1004"
+    tauDINFang = "../data/tauDEM/tauDINFang1005.tif" 
+    taufac = "../data/tauDEM/taufac1005.tif" 
+    invDist = "../data/tauDEM/invDist1005.tif" 
+    workDir = "../work/1005"
+    outDir = "../CPGs/1005"
+    logDir = "../logs/1005"
     cores = 20
     accumThresh = 1000
     overwrite = True
