ed803f45ea772654deb1af6b76f7707945654fb2
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-07-19 16:07:03 +0000

    Update batchDecayCPGs.py

--- batchDecayCPGs.py
+++ batchDecayCPGs.py
@@ -17,12 +17,12 @@ if len(sys.argv) > 1:
 else:
     #If inputs aren't specified in system args, set them in the script
     inDir = "../data/cov/NDVI_eulersZ/vrt" 
-    tauDINFang = "../data/tauDEM/tauDINFang1007.tif" 
-    taufac = "../data/tauDEM/taufac1007.tif" 
-    invDist = "../data/tauDEM/invDist1007.tif" 
-    workDir = "../work/1007"
-    outDir = "../CPGs/1007"
-    logDir = "../logs/1007"
+    tauDINFang = "../data/tauDEM/tauDINFang1008.tif" 
+    taufac = "../data/tauDEM/taufac1008.tif" 
+    invDist = "../data/tauDEM/invDist1008.tif" 
+    workDir = "../work/1008"
+    outDir = "../CPGs/1008"
+    logDir = "../logs/1008"
     cores = 20
     accumThresh = 1000
     overwrite = True
