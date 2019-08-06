e148c64ac1169fffb37c5fad792b80a005d4566d
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-08-06 16:54:30 +0000

    Chnage .vrt to .tif

--- nan2nodata.py
+++ nan2nodata.py
@@ -71,7 +71,7 @@ if os.path.isdir(inDir):
     for path, subdirs, files in os.walk(inDir):
         for name in files:
             #Check if file is .tif, and if so add it to covariate list
-            if os.path.splitext(name)[1] == ".vrt":
+            if os.path.splitext(name)[1] == ".tif":
                     covList.append(os.path.join(path, name))
 elif os.path.isfile(inDir):
     #Supplied path is a single covariate file
