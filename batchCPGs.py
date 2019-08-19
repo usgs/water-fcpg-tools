4ee3c7af06b422531a022b24fd24c1dd4290fa75
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-08-19 15:28:14 +0000

    Add missing SNODAS CPG

--- batchCPGs.py
+++ batchCPGs.py
@@ -15,12 +15,12 @@ if len(sys.argv) > 1:
     deleteTemp = sys.argv[10] #Whether to delete temporary files
 else:
     #If inputs aren't specified in system args, set them in the script
-    inDir = "../data/cov/gridMET_SOILMOISTmm" 
-    taufdr = "../data/tauDEM/taufdr1003.tif" 
-    taufac = "../data/tauDEM/taufac1003.tif" 
-    workDir = "../work/1003"
-    outDir = "../CPGs/1003"
-    logDir = "../logs/1003"
+    inDir = "../data/cov/SNODAS_SWEmm/SNODAS_SWEmm_2006_03_01.tif" 
+    taufdr = "../data/tauDEM/taufdr1002.tif" 
+    taufac = "../data/tauDEM/taufac1002.tif" 
+    workDir = "../work/1002"
+    outDir = "../CPGs/1002"
+    logDir = "../logs/1002"
     cores = 20
     accumThresh = 1000
     overwrite = True
