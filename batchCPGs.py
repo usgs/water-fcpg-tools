a1ed993cccf72c3cc74114de551e9822785b545f
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-08-19 22:36:44 +0000

    Update batchCPGs.py

--- batchCPGs.py
+++ batchCPGs.py
@@ -15,7 +15,7 @@ if len(sys.argv) > 1:
     deleteTemp = sys.argv[10] #Whether to delete temporary files
 else:
     #If inputs aren't specified in system args, set them in the script
-    inDir = "../data/cov/SNODAS_SWEmm/SNODAS_SWEmm_2006_07_01.tif" 
+    inDir = "../data/cov/static/Soils/SSURGO_AVGAWC.tif" 
     taufdr = "../data/tauDEM/taufdr1002.tif" 
     taufac = "../data/tauDEM/taufac1002.tif" 
     workDir = "../work/1002"
@@ -24,7 +24,7 @@ else:
     cores = 20
     accumThresh = 1000
     overwrite = True
-    deleteTemp = True
+    deleteTemp = False
 
 covList = [] #Initialize list of covariates
 
