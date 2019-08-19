d77b0496f139b99d1254b3139fde79ed9601e278
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-08-19 15:46:38 +0000

    Add other missing SNODAS CPG

--- batchCPGs.py
+++ batchCPGs.py
@@ -15,7 +15,7 @@ if len(sys.argv) > 1:
     deleteTemp = sys.argv[10] #Whether to delete temporary files
 else:
     #If inputs aren't specified in system args, set them in the script
-    inDir = "../data/cov/SNODAS_SWEmm/SNODAS_SWEmm_2006_03_01.tif" 
+    inDir = "../data/cov/SNODAS_SWEmm/SNODAS_SWEmm_2006_07_01.tif" 
     taufdr = "../data/tauDEM/taufdr1013.tif" 
     taufac = "../data/tauDEM/taufac1013.tif" 
     workDir = "../work/1013"
