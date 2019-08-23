23392325c10a4aeecbd32feaba177c1b0e8c10f5
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-08-23 19:20:07 +0000

    HUC 1003 SSURGO

--- batchCPGs.py
+++ batchCPGs.py
@@ -16,11 +16,11 @@ if len(sys.argv) > 1:
 else:
     #If inputs aren't specified in system args, set them in the script
     inDir = "../data/cov/static/Soils" 
-    taufdr = "../data/tauDEM/taufdr1013.tif" 
-    taufac = "../data/tauDEM/taufac1013.tif" 
-    workDir = "../work/1013"
-    outDir = "../CPGs/1013"
-    logDir = "../logs/1013"
+    taufdr = "../data/tauDEM/taufdr1003.tif" 
+    taufac = "../data/tauDEM/taufac1003.tif" 
+    workDir = "../work/1003"
+    outDir = "../CPGs/1003"
+    logDir = "../logs/1003"
     cores = 20
     accumThresh = 1000
     overwrite = True
