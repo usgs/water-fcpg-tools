e51a05cfb57bbd5c794569ecf249e0bab67f289b
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-08-26 16:11:09 +0000

    HUC 1003 CHILI

--- batchDecayCPGs.py
+++ batchDecayCPGs.py
@@ -18,16 +18,16 @@ else:
     #If inputs aren't specified in system args, set them in the script
     #inDir = "../data/cov/static/XXX_springs.tif" 
     inDir = "../data/cov/static/CHILI_10.vrt" 
-    tauDINFang = "../data/tauDEM/tauRADang1002.tif" 
-    strmRast = "../CPGs/1002/gridMET_minTempK_2017_12_00_HUC1002_CPG.tif" 
-    decayRast = "../data/tauDEM/oneFourthDecay1002.tif" 
-    workDir = "../work/1002"
-    outDir = "../CPGs/1002"
-    logDir = "../logs/1002"
+    tauDINFang = "../data/tauDEM/tauRADang1003.tif" 
+    strmRast = "../CPGs/1003/gridMET_minTempK_2017_12_00_HUC1003_CPG.tif" 
+    decayRast = "../data/tauDEM/oneFourthDecay1003.tif" 
+    workDir = "../work/1003"
+    outDir = "../CPGs/1003"
+    logDir = "../logs/1003"
     cores = 20
     accumThresh = 1000
     overwrite = True
-    deleteTemp = False
+    deleteTemp = True
 
 covList = [] #Initialize list of covariates
 
