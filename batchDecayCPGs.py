b57efb91d902481dff65025a537754b1304345de
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-08-26 18:40:47 +0000

    HUC 1011 springs

--- batchDecayCPGs.py
+++ batchDecayCPGs.py
@@ -18,12 +18,12 @@ else:
     #If inputs aren't specified in system args, set them in the script
     inDir = "../data/cov/static/XXX_springs.tif" 
     #inDir = "../data/cov/static/CHILI_10.vrt" 
-    tauDINFang = "../data/tauDEM/tauRADang1010.tif" 
-    strmRast = "../CPGs/1010/gridMET_minTempK_2017_12_00_HUC1010_CPG.tif" 
-    decayRast = "../data/tauDEM/oneFourthDecay1010.tif" 
-    workDir = "../work/1010"
-    outDir = "../CPGs/1010"
-    logDir = "../logs/1010"
+    tauDINFang = "../data/tauDEM/tauRADang1011.tif" 
+    strmRast = "../CPGs/1011/gridMET_minTempK_2017_12_00_HUC1011_CPG.tif" 
+    decayRast = "../data/tauDEM/oneFourthDecay1011.tif" 
+    workDir = "../work/1011"
+    outDir = "../CPGs/1011"
+    logDir = "../logs/1011"
     cores = 20
     accumThresh = 1000
     overwrite = True
