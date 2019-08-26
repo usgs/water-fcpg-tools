c7ed1ea0c3c6cd881b18d5215db46e528b5f7e31
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-08-26 13:47:47 +0000

    Code for springs raster

--- batchDecayCPGs.py
+++ batchDecayCPGs.py
@@ -16,18 +16,17 @@ if len(sys.argv) > 1:
     deleteTemp = sys.argv[11] #Whether to delete temporary files
 else:
     #If inputs aren't specified in system args, set them in the script
-    inDir = "../data/cov/landsat_NDVI-May-Oct2/vrt" 
-    #inDir = "../data/cov/static/springs_rast.tif" 
-    tauDINFang = "../data/tauDEM/tauRADang1013.tif" 
-    strmRast = "../CPGs/1013/gridMET_minTempK_2017_12_00_HUC1013_CPG.tif" 
-    decayRast = "../data/tauDEM/oneFourthDecay1013.tif" 
-    workDir = "../work/1013"
-    outDir = "../CPGs/1013"
-    logDir = "../logs/1013"
+    inDir = "../data/cov/static/XXX_springs.tif" 
+    tauDINFang = "../data/tauDEM/tauRADang1002.tif" 
+    strmRast = "../CPGs/1002/gridMET_minTempK_2017_12_00_HUC1002_CPG.tif" 
+    decayRast = "../data/tauDEM/oneFourthDecay1002.tif" 
+    workDir = "../work/1002"
+    outDir = "../CPGs/1002"
+    logDir = "../logs/1002"
     cores = 20
     accumThresh = 1000
     overwrite = True
-    deleteTemp = False
+    deleteTemp = True
 
 covList = [] #Initialize list of covariates
 
