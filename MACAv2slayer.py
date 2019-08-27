bc8e9cf42745a653eb54f940c719585b26d0c9cc
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-08-27 15:52:19 +0000

    Code to convert netCDF to tiff

--- MACAv2slayer.py
+++ MACAv2slayer.py
@@ -9,6 +9,9 @@ import traceback
 # Must have gdal and nco tools (module load tools/nco-4.7.8-gnu) modules loaded 
 
 inDir = "../data/cov/macav2metdata" #Directory with netCDF files
+outDir = "../data/cov/MACAv2"
+dataSource = MACAv2 #Name of data source
+
 #reorderCDF = "../data/cov/soil_gridMETfix.nc" #NetCDF file with reordered dimensions
 #multiTIFF = "../data/cov/gridMET_SOILMOISTmm.tif" #Multiband .tif created from netCDF
 
@@ -33,76 +36,73 @@ else:
 for cov in covList:
     
     baseName = os.path.basename(cov)
+    param = baseName.split("_")[1] #Get the parameter name
+    startTime = baseName.split("_")[6] #Get the file start year
+    endTime = baseName.split("_")[7] #Get the file start year
+
+    reorderCDF = os.path.join(outDir, "{0}_{1}_{2}_{3}.nc".format(dataSource, param, startTime, endTime))
+
+    multiTIFF = os.path.join(outDir, "{0}_{1}_{2}_{3}.tif".format(dataSource, param, startTime, endTime))
+
+
     print(baseName)
+    print(reorderCDF)
 
+    #Step 1: Put the file dimensions in the correct order
 
-
-
-
-"""
-
-baseName = "gridMET_SOILMOISTmm"
-
-outDir = "../data/cov/gridMET_SOILMOISTmm"
-
-#Step 1: Put the file dimensions in the correct order
-
-try:
-        cmd = "ncpdq -a time,lat,lon {0} {1}".format(inCDF, reorderCDF)
+    try:
+        cmd = "ncpdq -a time,lat,lon {0} {1}".format(cov, reorderCDF)
         result = subprocess.run(cmd, shell = True)
         result.stdout
         
-except:
+    except:
         print('Error reordering NetCDF dimensions')
         traceback.print_exc()
+    
+
+    #Step 2: Convert the netCDF to a multiband GeoTIFF
 
 
-#Step 2: Convert the netCDF to a multiband GeoTIFF
-
-
-try:
+    try:
         cmd = "gdal_translate -of GTiff -a_srs EPSG:4326 {0} {1}".format(reorderCDF, multiTIFF)
         result = subprocess.run(cmd, shell = True)
         result.stdout
         
-except:
+    except:
         print('Error converting netCDF to geoTIFF')
         traceback.print_exc()
 
-#Step 3: Convert each band of the GeoTIFF to its own raster
 
 
-#netCDFpath = "../data/cov/gridMET_PRmm.tif"
+    #Step 3: Convert each band of the GeoTIFF to its own raster
 
-
-
-with rs.open(multiTIFF) as ds: # load parameter raster
+    with rs.open(multiTIFF) as ds: # load parameter raster
         numBands = ds.count
         data = ds.read()
         profile = ds.profile
         paramNoData = ds.nodata
         tags = ds.tags()
 
-print(profile)
+    print(profile)
 
-day0 = datetime.datetime.strptime("01-01-1900", "%d-%m-%Y") #Set the day time is counted from
-
-
-days = tags["NETCDF_DIM_time_VALUES"] #Get the list of dates associated with each band and convert to list
-days = days.replace("{", "")
-days = days.replace("}", "")
-days = days.split(",")
+    day0 = datetime.datetime.strptime("01-01-1900", "%d-%m-%Y") #Set the day time is counted from
 
 
-i = 0 
+    days = tags["NETCDF_DIM_time_VALUES"] #Get the list of dates associated with each band and convert to list
+    days = days.replace("{", "")
+    days = days.replace("}", "")
+    days = days.split(",")
 
-for band in data:
+
+    i = 0 
+
+    for band in data:
 
         day = int(days[i]) #Get the days since beginning associated with the band
         
         date = day0 + datetime.timedelta(days=day) #Compute the date associated with the band
 
-        fileName = os.path.join(outDir, baseName + "_" + date.strftime('%Y_%m_%d') + ".tif") #Create the name for the output file
+        fileName = os.path.join(outDir, dataSource + "_" + param + "_" + date.strftime('%Y_%m_%d') + ".tif") #Create the name for the output file
 
         #Update raster profile
         profile.update({
