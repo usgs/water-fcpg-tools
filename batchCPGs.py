5f3c618d086a980e4fec7fe9389e5327106bdee5
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-11-20 14:29:52 +0000

    HUC 1003 ET

--- batchCPGs.py
+++ batchCPGs.py
@@ -16,11 +16,11 @@ if len(sys.argv) > 1:
 else:
     #If inputs aren't specified in system args, set them in the script
     inDir = "../data/cov/landsatET/vrt" 
-    taufdr = "../data/tauDEM/taufdr1002.tif" 
-    taufac = "../data/tauDEM/taufac1002.tif" 
-    workDir = "../work/1002"
-    outDir = "../CPGs/1002"
-    logDir = "../logs/1002"
+    taufdr = "../data/tauDEM/taufdr1003.tif" 
+    taufac = "../data/tauDEM/taufac1003.tif" 
+    workDir = "../work/1003"
+    outDir = "../CPGs/1003"
+    logDir = "../logs/1003"
     cores = 20
     accumThresh = 1000
     overwrite = True
