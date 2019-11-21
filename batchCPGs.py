76b1c3c632928982a164973d44dde6824d352093
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-11-21 15:36:48 +0000

    HUC 1002 EffPrec

--- batchCPGs.py
+++ batchCPGs.py
@@ -15,12 +15,12 @@ if len(sys.argv) > 1:
     deleteTemp = sys.argv[10] #Whether to delete temporary files
 else:
     #If inputs aren't specified in system args, set them in the script
-    inDir = "../data/cov/landsatET/vrt/landsat_ETmm_2003_00_00.vrt" 
-    taufdr = "../data/tauDEM/taufdr1011.tif" 
-    taufac = "../data/tauDEM/taufac1011.tif" 
-    workDir = "../work/1011"
-    outDir = "../CPGs/1011"
-    logDir = "../logs/1011"
+    inDir = "../data/cov/landsatEffPrec/vrt" 
+    taufdr = "../data/tauDEM/taufdr1002.tif" 
+    taufac = "../data/tauDEM/taufac1002.tif" 
+    workDir = "../work/1002"
+    outDir = "../CPGs/1002"
+    logDir = "../logs/1002"
     cores = 20
     accumThresh = 1000
     overwrite = True
