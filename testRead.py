4f94ef5d23719360eb2d04a750da304e907c9fc1
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-11-22 19:26:53 +0000

    Update testRead.py

--- testRead.py
+++ testRead.py
@@ -4,8 +4,13 @@ import datetime as dt
 import rasterio as rs
 import os
 import sys
+import gdal
 #import re
 np.set_printoptions(threshold=sys.maxsize)
+
+ds = gdal.Open(templateFile)
+a = ds.ReadAsArray()
+nx,ny = np.shape(a)
 
 
 inRast = '../CPGs/nc/testInput/gridMET_minTempK_1979_01_00_HUC1002_CPG.tif'
@@ -18,7 +23,7 @@ with rs.open(inRast) as ds:
         profile = ds.profile.copy() # save the metadata for output later
 
 
-print(dat[5000:5005, 5000:5005])
+print(dat[5005:5010, 5005:5010])
 
 with rs.open(outRast,'w',**profile) as dst:
         dst.write(dat,1)
