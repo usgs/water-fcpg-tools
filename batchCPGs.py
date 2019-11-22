420d5121ed0a8ddd506a0035a31873d4b2c35a33
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-11-22 20:38:19 +0000

    Rerun HUC 1013 2003 ET

--- batchCPGs.py
+++ batchCPGs.py
@@ -15,7 +15,7 @@ if len(sys.argv) > 1:
     deleteTemp = sys.argv[10] #Whether to delete temporary files
 else:
     #If inputs aren't specified in system args, set them in the script
-    inDir = "../data/cov/landsatEffPrec/vrt" 
+    inDir = "../data/cov/landsatET/vrt/landsat_ETmm_2003_00_00.vrt" 
     taufdr = "../data/tauDEM/taufdr1013.tif" 
     taufac = "../data/tauDEM/taufac1013.tif" 
     workDir = "../work/1013"
