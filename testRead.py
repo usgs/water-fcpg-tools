98d4f98c44303391384171be49b82ca9673d27e4
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-11-22 19:20:18 +0000

    Script to test reading of CPGs

---
+++ testRead.py
@@ -0,0 +1,26 @@
+
+import numpy as np
+import datetime as dt
+import rasterio as rs
+import os
+import gdal
+import netCDF4
+import sys
+#import re
+
+inRast = '../CPGs/nc/testInput/gridMET_minTempK_1979_01_00_HUC1002_CPG.tif'
+outRast = '../CPGs/nc/testRead.tif'
+
+
+with rs.open(inRast) as ds:
+        dat = ds.read(1)
+        inNoData = ds.nodata
+        profile = ds.profile.copy() # save the metadata for output later
+
+
+print(dat[5000:6000, 5000:6000])
+
+with rs.open(outRast,'w',**profile) as dst:
+        dst.write(dat,1)
+        
+
