cbf95398944f6de708ec250bf419a8d902620e57
Siefken, Seth Arthur <ssiefken@usgs.gov>
2019-12-13 16:38:25 +0000

    Rerun 1990 Eff Precip in HUC 1012

--- batchCPGs.py
+++ batchCPGs.py
@@ -15,12 +15,12 @@ if len(sys.argv) > 1:
     deleteTemp = sys.argv[10] #Whether to delete temporary files
 else:
     #If inputs aren't specified in system args, set them in the script
-    inDir = "../data/cov/landsatEffPrec/vrt/landsat_EffPrecmm_2012_00_00.vrt" 
-    taufdr = "../data/tauDEM/taufdr1011.tif" 
-    taufac = "../data/tauDEM/taufac1011.tif" 
-    workDir = "../work/1011"
-    outDir = "../CPGs/1011"
-    logDir = "../logs/1011"
+    inDir = "../data/cov/landsatEffPrec/vrt/landsat_EffPrecmm_1990_00_00.vrt" 
+    taufdr = "../data/tauDEM/taufdr1012.tif" 
+    taufac = "../data/tauDEM/taufac1012.tif" 
+    workDir = "../work/1012"
+    outDir = "../CPGs/1012"
+    logDir = "../logs/1012"
     cores = 20
     accumThresh = 1000
     overwrite = True
