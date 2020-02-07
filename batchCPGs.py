e52b410a47e055fc048f13b9005d84f2cbc831c1
Siefken, Seth Arthur <ssiefken@usgs.gov>
2020-02-07 16:08:25 +0000

    HUC 1004 Temp CPGs

--- batchCPGs.py
+++ batchCPGs.py
@@ -16,11 +16,11 @@ if len(sys.argv) > 1:
 else:
     #If inputs aren't specified in system args, set them in the script
     inDir = "../data/cov/gridMET_minTempK/2018" 
-    taufdr = "../data/tauDEM/taufdr1003.tif" 
-    taufac = "../data/tauDEM/taufac1003.tif" 
-    workDir = "../work/1003"
-    outDir = "../CPGs/1003"
-    logDir = "../logs/1003"
+    taufdr = "../data/tauDEM/taufdr1004.tif" 
+    taufac = "../data/tauDEM/taufac1004.tif" 
+    workDir = "../work/1004"
+    outDir = "../CPGs/1004"
+    logDir = "../logs/1004"
     cores = 20
     accumThresh = 1000
     overwrite = True
