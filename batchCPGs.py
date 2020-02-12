3a2c3e9a8f4cf994a3edfe15292e97e76c3af47f
Siefken, Seth Arthur <ssiefken@usgs.gov>
2020-02-12 22:03:38 +0000

    HUC 1007 2018 gridmet

--- batchCPGs.py
+++ batchCPGs.py
@@ -16,11 +16,11 @@ if len(sys.argv) > 1:
 else:
     #If inputs aren't specified in system args, set them in the script
     inDir = "../data/cov/GRIDMET_DATA_2018" 
-    taufdr = "../data/tauDEM/taufdr1006.tif" 
-    taufac = "../data/tauDEM/taufac1006.tif" 
-    workDir = "../work/1006"
-    outDir = "../CPGs/1006"
-    logDir = "../logs/1006"
+    taufdr = "../data/tauDEM/taufdr1007.tif" 
+    taufac = "../data/tauDEM/taufac1007.tif" 
+    workDir = "../work/1007"
+    outDir = "../CPGs/1007"
+    logDir = "../logs/1007"
     cores = 20
     accumThresh = 1000
     overwrite = True
