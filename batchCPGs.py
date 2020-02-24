b92195fd4cbcb59f6ae0ee6d86e9792cf842c574
Siefken, Seth Arthur <ssiefken@usgs.gov>
2020-02-24 16:16:32 +0000

    HUC 1013 elevation CPG

--- batchCPGs.py
+++ batchCPGs.py
@@ -15,12 +15,12 @@ if len(sys.argv) > 1:
     deleteTemp = sys.argv[10] #Whether to delete temporary files
 else:
     #If inputs aren't specified in system args, set them in the script
-    inDir = "../data/cov/static/elev_cm_1012.tif" 
-    taufdr = "../data/tauDEM/taufdr1012.tif" 
-    taufac = "../data/tauDEM/taufac1012.tif" 
-    workDir = "../work/1012"
-    outDir = "../CPGs/1012"
-    logDir = "../logs/1012"
+    inDir = "../data/cov/static/elev_cm_1013.tif" 
+    taufdr = "../data/tauDEM/taufdr1013.tif" 
+    taufac = "../data/tauDEM/taufac1013.tif" 
+    workDir = "../work/1013"
+    outDir = "../CPGs/1013"
+    logDir = "../logs/1013"
     cores = 20
     accumThresh = 1000
     overwrite = True
