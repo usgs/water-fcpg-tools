d608b0e0aa2849d806b38279c5533ddfceb1930a
ssiefken <ssiefken@usgs.gov>
2020-03-06 13:17:48 -0700

    Remove PROSPER specific scripts

--- wkt2proj4_NHDplusV21.py
+++
@@ -1,7 +0,0 @@
-from osgeo import osr
-srs = osr.SpatialReference()
-
-wkt_text = 'PROJCS["unnamed",GEOGCS["GRS 1980(IUGG, 1980)",DATUM["unknown",SPHEROID["GRS80",6378137,298.257222101],TOWGS84[0,0,0,0,0,0,0]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]],PROJECTION["Albers_Conic_Equal_Area"],PARAMETER["standard_parallel_1",29.5],PARAMETER["standard_parallel_2",45.5],PARAMETER["latitude_of_center",23],PARAMETER["longitude_of_center",-96],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]]]'
-
-srs.ImportFromWkt(wkt_text)
-srs.ExportToProj4()
