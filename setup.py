661eb70281502e4f6bc4ab6d0d64f023728c2406
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2022-09-23 17:07:46 -0400

    Updating setup.py (and version #s)
    * Replaced the url in `setup.py` with the updated url.
    * Updated the `classifiers` variable with all python version >= 3.7, as well as topic tags, and appropriate OS tags (see https://pypi.org/classifiers/)
    * Updated all references to the python version to >= 3.7.
    * Updated the project description to capital FCPG including the acronym for clarity.

--- setup.py
+++ setup.py
@@ -13,20 +13,19 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-# test the python version
-major, minor = sys.version_info[0:2]
-if (major, minor) < (3,7):
-	sys.stderr.write('\nPython 3.7 or later is required for this package.\n')
-	sys.exit(1)
-
-
 # Get version from __init__.py
 from FCPGtools import __version__
 
+# test the python version
+major, minor = sys.version_info[0:2]
+if (major, minor) >= (3,7):
+	sys.stderr.write('\nPython 3.7 or later is required for this package.\n')
+	sys.exit(1)
+
+# get absolute path to this directory
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-
 with open(path.join(here, 'README.rst'), encoding = 'utf-8') as f:
 	long_description = f.read()
 
@@ -35,15 +34,22 @@ setup(
 	version = __version__,
 	author = "Theodore Barnhart",
 	author_email = "tbarnhart@usgs.gov",
-	description = "Tools to make flow-conditioned parameter grids.",
+	description = "Tools to make Flow-Conditioned Parameter Grids (FCPG)",
 	long_description = long_description,
 	long_description_content_type = "text/x-rst",
-	url="https://code.usgs.gov/StreamStats/CPGtools",
+	url="https://code.usgs.gov/StreamStats/data-preparation/cpg/FCPGtools",
 	packages = find_packages(),
 	classifiers = [
-		"Programming Language :: Python :: 3",
-		"Operating System :: Linux",
+		"Natural Language :: English",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: 3.10",
+		"Operating System :: POSIX :: Linux",
+		"Operating System :: Microsoft :: Windows",
 		"Development Status :: 5 - Production/Stable",
+		"Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
 		"License :: OSI Approved :: MIT License",
 	],
 	python_requires = '>=3.7',
