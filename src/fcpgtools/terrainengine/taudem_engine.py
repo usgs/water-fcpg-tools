Fixed silent error in tools.align_raster()

src/fcpgtools/terrainengine/taudem_engine.py
@@ -10,8 +10,9 @@ import numpy as np
 import xarray as xr
 from fcpgtools.types import Raster, TauDEMDict
 from fcpgtools.utilities import intake_raster, save_raster, _verify_shape_match, \
-    _combine_split_bands, _split_bands, update_parameter_raster, _prep_parameter_grid, \
+    _combine_split_bands, _split_bands, update_parameter_raster, \
     _replace_nodata_value
+from fcpgtools.tools import prep_parameter_grid
 
 def _taudem_prepper(
     in_raster: Raster,
@@ -73,7 +74,6 @@ def fac_from_fdr(
     :param **kwargs: can pass in optional values using "cores", "mpiCall", "mpiArg" TauDem arguments.
     :returns: (xr.DataArray) the Flow Accumulation Cells (FAC) raster as a xarray DataArray object.
     """
-    d8_fdr = intake_raster(d8_fdr)
     d8_fdr_path = _taudem_prepper(d8_fdr)
 
     # get tempoarary files for necessary inputs
@@ -92,7 +92,7 @@ def fac_from_fdr(
                 weights,
                 upstream_pour_points,
                 )
-            weights = _prep_parameter_grid(
+            weights = prep_parameter_grid(
                 weights,
                 d8_fdr,
                 -1,
@@ -125,17 +125,13 @@ def fac_from_fdr(
     taudem_dict = _update_taudem_dict(taudem_dict, kwargs)
 
     # use TauDEM via subprocess to make a Flow Accumulation Raster
-    try:
-        cmd = '{mpiCall} {mpiArg} {cores} aread8 -p {fdr} -ad8 {outFl} {finalArg}'.format(
-            **taudem_dict)
-        _ = subprocess.run(cmd, shell=True)
-
-    except Exception:
-        #TODO: Handle exceptions
-        print('ERROR: TauDEM AreaD8 failed!')
-        traceback.print_exc()
+    cmd = '{mpiCall} {mpiArg} {cores} aread8 -p {fdr} -ad8 {outFl} {finalArg}'.format(
+        **taudem_dict)
+    _ = subprocess.run(cmd, shell=True)
     
-    print(taudem_dict['outFl'])
+    if not Path(taudem_dict['outFl']).exists():
+        raise FileNotFoundError('ERROR: TauDEM areaD8 failed to create an output!')
+
     out_raster = intake_raster(Path(taudem_dict['outFl']))
     out_raster = out_raster.astype(np.float64)
 
@@ -174,9 +170,9 @@ def parameter_accumulate(
     :returns: (xr.DataArray) the parameter accumulation raster as a xarray DataArray object.
     """
     d8_fdr = intake_raster(d8_fdr)
-    parameter_raster = _prep_parameter_grid(
+    parameter_raster = prep_parameter_grid(
         parameter_raster=parameter_raster,
-        d8_fdr=d8_fdr,
+        fdr_raster=d8_fdr,
         out_of_bounds_value=-1,
         )
 
@@ -366,8 +362,8 @@ def decay_accumulation(
     # prep parameter raster is applicable
     if parameter_raster is not None:
         parameter_raster = intake_raster(parameter_raster)
-        parameter_raster = _prep_parameter_grid(
-            _prep_parameter_grid,
+        parameter_raster = prep_parameter_grid(
+            prep_parameter_grid,
             out_of_bounds_value=-1
         )
         parameter_raster_path = _taudem_prepper(parameter_raster)

src/fcpgtools/tools.py
@@ -3,10 +3,11 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 import geopandas as gpd
+from rasterio.enums import Resampling
 from fcpgtools.types import Raster, FDRD8Formats, D8ConversionDicts, PourPointLocationsDict, PourPointValuesDict
 from fcpgtools.utilities import intake_raster, intake_shapefile, save_raster, clip, reproject_raster, \
     resample, id_d8_format, _combine_split_bands, _verify_basin_coverage, get_max_cell, sample_raster, \
-    _split_bands
+    _split_bands, _verify_shape_match, _get_crs, _replace_nodata_value, _verify_alignment
 
 # CLIENT FACING FUNCTIONS
 def align_raster(
@@ -16,20 +17,13 @@ def align_raster(
     out_path: str = None,
     ) -> xr.DataArray:
 
-    out_raster = clip(
-        in_raster,
+    out_raster = in_raster.rio.reproject_match(
         match_raster,
+        resampling=getattr(Resampling, resample_method),
         )
-    out_raster = reproject_raster(
-        in_raster,
-        out_crs=match_raster,
-        )
-    out_raster = resample(
-        in_raster,
-        match_raster,
-        method=resample_method,
-        out_path=out_path,
-        )
+    
+    if out_path is not None:
+        save_raster(out_raster, out_path)
     return out_raster
 
 def convert_fdr_formats(
@@ -89,6 +83,56 @@ def convert_fdr_formats(
     print(f'Converted the D8 Flow Direction Raster (FDR) from {in_format} format'
     f' to {out_format}')
     return d8_fdr
+
+def prep_parameter_grid(
+    parameter_raster: xr.DataArray,
+    fdr_raster: xr.DataArray,
+    out_of_bounds_value: Union[float, int],
+    ) -> xr.DataArray:
+
+    # check that shapes match
+    if not _verify_shape_match(fdr_raster, parameter_raster):
+        print('ERROR: The D8 FDR raster and the parameter raster must have the same shape. '
+        'Please run fcpgtools.tools.align_raster(d8_fdr, parameter_raster).')
+        raise TypeError
+
+    # use a where query to replace out of bounds values
+    og_nodata = parameter_raster.rio.nodata
+    og_crs = _get_crs(parameter_raster)
+    out_crs = _get_crs(fdr_raster)
+
+    if not _verify_alignment(parameter_raster, fdr_raster):
+        parameter_raster = align_raster(
+            parameter_raster,
+            fdr_raster,
+            resample_method='nearest',
+            )
+
+    parameter_raster = parameter_raster.where(
+        fdr_raster.values != fdr_raster.rio.nodata,
+        out_of_bounds_value,
+        )
+
+    # convert in-bounds nodata to 0
+    if np.isin(og_nodata, parameter_raster.values):
+        parameter_raster = parameter_raster.where(
+            (fdr_raster.values == fdr_raster.rio.nodata) & \
+                (parameter_raster.values != og_nodata),
+            0,
+            )
+
+    # update nodata and crs
+    parameter_raster.rio.write_nodata(
+        og_nodata,
+        inplace=True,
+        )
+    parameter_raster.rio.write_crs(og_crs, inplace=True)
+    parameter_raster = _replace_nodata_value(
+        parameter_raster,
+        out_of_bounds_value,
+        )
+
+    return parameter_raster
 
 # raster masking function
 def spatial_mask(

src/fcpgtools/utilities.py
@@ -142,47 +142,6 @@ def _verify_dtype(
         #TODO: How best to handle other int dtype? What are the value ranges.
         return True
 
-def _prep_parameter_grid(
-    parameter_raster: xr.DataArray,
-    fdr_raster: xr.DataArray,
-    out_of_bounds_value: Union[float, int],
-    ) -> xr.DataArray:
-
-    # check that shapes match
-    if not _verify_shape_match(fdr_raster, parameter_raster):
-        print('ERROR: The D8 FDR raster and the parameter raster must have the same shape. '
-        'Please run fcpgtools.tools.align_raster(d8_fdr, parameter_raster).')
-        raise TypeError
-
-    # use a where query to replace out of bounds values
-    og_nodata = parameter_raster.rio.nodata
-    og_crs = _get_crs(parameter_raster)
-    parameter_raster = parameter_raster.where(
-        fdr_raster.values != fdr_raster.rio.nodata,
-        out_of_bounds_value,
-        )
-
-    # convert in-bounds nodata to 0
-    if np.isin(og_nodata, parameter_raster.values):
-        parameter_raster = parameter_raster.where(
-            (fdr_raster.values == fdr_raster.rio.nodata) & \
-                (parameter_raster.values != og_nodata),
-            0,
-            )
-
-    # update nodata and crs
-    parameter_raster.rio.write_nodata(
-        og_nodata,
-        inplace=True,
-        )
-    parameter_raster.rio.write_crs(og_crs, inplace=True)
-    parameter_raster = _replace_nodata_value(
-        parameter_raster,
-        out_of_bounds_value,
-        )
-
-    return parameter_raster
-
 def _update_cell_value_(
     raster: xr.DataArray,
     coords: Tuple[float, float],
@@ -384,7 +343,7 @@ def clip(
     if match_raster is not None:
         match_raster = intake_raster(match_raster)
         crs = match_raster.rio.crs.to_wkt()
-        bbox = list(match_raster.rio.bounds())
+        bbox = match_raster.rio.bounds()
     elif match_shapefile is not None:
         match_shapefile = intake_shapefile(match_shapefile)
         crs = match_shapefile.crs.to_wkt()
@@ -400,6 +359,7 @@ def clip(
         maxy=bbox[3],
         crs=crs,
         )
+
     if out_path is not None:
         save_raster(out_raster, out_path)
 
@@ -408,6 +368,7 @@ def clip(
 def reproject_raster(
     in_raster: Raster,
     out_crs: Union[Raster, Shapefile] = None,
+    resolution: Union[float, Tuple[float, float]] = None,
     wkt_string: str = None,
     out_path: str = None,
     ) -> xr.DataArray:
@@ -416,6 +377,7 @@ def reproject_raster(
     :param in_raster: (xr.DataArray or str raster path) input raster.
     :param out_crs: (xr.DataArray, gpd.GeoDataFrame, or string file path) the output CRS,
         defined by either copying another raster or shapefile's CRS.
+    :param resolution: (float or Tuple[float, float], default=None) allows the output resolution to be overriden.
     :param wkt_string: (str, valid CRS WKT - default is None) allows the user to pass in a custom WKT string.
     :param out_path: (str, default=None) defines a path to save the output raster.
     :returns: (xr.DataArray) the reprojected raster as a xarray DataArray object.
@@ -425,13 +387,15 @@ def reproject_raster(
         out_crs = _get_crs(out_crs)
     elif wkt_string is not None:
         out_crs = wkt_string
+        resolution = None
     else:
         #TODO: Update exceptions
         print('Must pass in either param:out_crs or param:wkt_string')
         return None
-        
+    
     out_raster = in_raster.rio.reproject(
         out_crs,
+        resolution=resolution,
         nodata=in_raster.rio.nodata,
         kwargs={
             'dst_nodata': in_raster.rio.nodata,
@@ -609,3 +573,24 @@ def _verify_basin_coverage(
     diff = np.array(raster_bbox) - np.array(shp_bbox)
     compare = np.sign(diff) == np.array([-1, -1, 1, 1])
     return np.all(compare)
+
+def _verify_alignment(
+    raster1: xr.DataArray,
+    raster2: xr.DataArray,
+    ) -> bool:
+    """
+    Returns True if the CRS, shape, and bbox of param:raster1 and param:raster2 match.
+    Note: This only works for two 2D rasters!
+    """
+    raster_list = [raster1, raster2]
+    for i, raster in enumerate(raster_list):
+        if len(raster.shape) == 3:
+            raster_list[i] = raster[1,:]
+
+    if raster1.rio.crs == raster2.rio.crs and raster_list[0].shape == raster_list[1].shape:
+        diff = np.array(raster_list[0].rio.bounds()) - \
+            np.array(raster_list[1].rio.bounds())
+        if np.max(diff) == 0: return True
+    return False
+        
+