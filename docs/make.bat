523ff619c8984fba12b6532e0f7ec342849b5de4
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-01-19 23:43:58 -0500

    Change make.bat env

--- docs/make.bat
+++ docs/make.bat
@@ -5,7 +5,7 @@ pushd %~dp0
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD="C:\Users\xrnogueira\Miniconda3\envs\fcpgtools\Scripts\sphinx-build.exe"
+	set SPHINXBUILD="C:\Users\xrnogueira\Miniconda3\envs\fcpgtools_dev\Scripts\sphinx-build.exe"
 )
 set SOURCEDIR=source
 set BUILDDIR=build
