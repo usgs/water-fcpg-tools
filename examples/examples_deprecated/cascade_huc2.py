6a9b4df04ead09641abdfe0b7c32ce0cfd19c9b3
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-01-24 10:53:01 -0500

    updated import to fcpgtools.deprecated for deprecated examples

--- examples/examples_deprecated/cascade_huc2.py
+++ examples/examples_deprecated/cascade_huc2.py
@@ -1,17 +1,18 @@
-import FCPGtools as fcpg # Import the tools. 
+import fcpgtools.tools_deprecated as fcpg  # Import the tools.
 
 # Define the file input and output paths and the upstream region.
-upstreamFDRflesri = '' # Upstream flow direction grid in ESRI format.
-downstreamFDRflesri = '' # Downstream flow direction grid in ESRI format.
-upstreamFACfltau = '' # Upstream flow accumulation grid from TauDEM.
-upstreamFDRfltau = '' # Upstream flow direction grid in TauDEM format.
-downstreamFACfltau = '' # Downstream flow accumuation grid from TauDEM.
-downstreamFDRfltau = '' # Downstream flow direction grid in TauDEM format.
-region = '' # Hydrologic region to make the update dictionary for.
-updateDict = '' # Path to the update dictionary to create.
-downstreamFACWeight = '' # Path to the downstream FAC weighting grid. This is where the upstream value will be inserted.
-downstreamAdjFAC = '' # Path to output the adjusted FAC weighting grid.
-cores = 8 #Number of cores to use for fcpg.adjustFAC() function
+upstreamFDRflesri = ''  # Upstream flow direction grid in ESRI format.
+downstreamFDRflesri = ''  # Downstream flow direction grid in ESRI format.
+upstreamFACfltau = ''  # Upstream flow accumulation grid from TauDEM.
+upstreamFDRfltau = ''  # Upstream flow direction grid in TauDEM format.
+downstreamFACfltau = ''  # Downstream flow accumuation grid from TauDEM.
+downstreamFDRfltau = ''  # Downstream flow direction grid in TauDEM format.
+region = ''  # Hydrologic region to make the update dictionary for.
+updateDict = ''  # Path to the update dictionary to create.
+# Path to the downstream FAC weighting grid. This is where the upstream value will be inserted.
+downstreamFACWeight = ''
+downstreamAdjFAC = ''  # Path to output the adjusted FAC weighting grid.
+cores = 8  # Number of cores to use for fcpg.adjustFAC() function
 
 # convert ESRI flow directions to TauDEM flow directions.
 fcpg.tauDrainDir(upstreamFDRflesri, upstreamFDRfltau)
@@ -21,12 +22,12 @@ fcpg.tauDrainDir(downstreamFDRflesri, downstreamFDRfltau)
 fcpg.tauFlowAccum(upstreamFDRfltau, upstreamFACfltau)
 
 # Create the update dictionary to move data between HUC2 regions.
-x,y,d,w = fcpg.findLastFACFD(upstreamFACfltau, upstreamFACfltau)
-fcpg.createUpdateDict(x,y,d,region,updateDict)
+x, y, d, w = fcpg.findLastFACFD(upstreamFACfltau, upstreamFACfltau)
+fcpg.createUpdateDict(x, y, d, region, updateDict)
 
 # Make a grid of ones based on the downstream grid.
-fcpg.makeFACweight(downstreamFDRfltau,downstreamFACWeight)
+fcpg.makeFACweight(downstreamFDRfltau, downstreamFACWeight)
 
 # Create the updated flow accumulation grid with information from the upstream HUC inserted into the source grid.
-fcpg.adjustFAC(downstreamFDRfltau,downstreamFACWeight,
-              updateDict,downstreamFDRfltau,downstreamAdjFAC,cores=cores)
+fcpg.adjustFAC(downstreamFDRfltau, downstreamFACWeight,
+               updateDict, downstreamFDRfltau, downstreamAdjFAC, cores=cores)
--- examples/examples_deprecated/cascade_huc4.py
+++ examples/examples_deprecated/cascade_huc4.py
@@ -1,38 +1,43 @@
-import FCPGtools as fcpg # Import the tools. 
+import fcpgtools.tools_deprecated as fcpg  # Import the tools.
 import geopandas as gpd
 
 # Define the file input and output paths and the upstream region.
-upstreamFACfltau = '' # Upstream flow accumulation grid from TauDEM.
-upstreamFDRfltau = '' # Upstream flow direction grid in TauDEM format.
-downstreamFACfltau = '' # Downstream flow accumuation grid from TauDEM.
-downstreamFDRfltau = '' # Downstream flow direction grid in TauDEM format.
-updateDict = '' # Path to the update dictionary to create.
-downstreamFACWeight = '' # Path to the downstream FAC weighting grid. This is where the upstream value will be inserted.
-downstreamAdjFAC = '' # Path to output the adjusted FAC weighting grid.
-wbdPth = '' # Path to the watershed boundary dataset for the HUC-2 region you are working in.
-cores = 6 #Number of cores to use for fcpg.adjustFAC() function
+upstreamFACfltau = ''  # Upstream flow accumulation grid from TauDEM.
+upstreamFDRfltau = ''  # Upstream flow direction grid in TauDEM format.
+downstreamFACfltau = ''  # Downstream flow accumuation grid from TauDEM.
+downstreamFDRfltau = ''  # Downstream flow direction grid in TauDEM format.
+updateDict = ''  # Path to the update dictionary to create.
+# Path to the downstream FAC weighting grid. This is where the upstream value will be inserted.
+downstreamFACWeight = ''
+downstreamAdjFAC = ''  # Path to output the adjusted FAC weighting grid.
+# Path to the watershed boundary dataset for the HUC-2 region you are working in.
+wbdPth = ''
+cores = 6  # Number of cores to use for fcpg.adjustFAC() function
 
 # Define upstream and downstream basins.
 upstream = ''
 downstream = ''
 
 # Load the WBD.
-layer = 'WBDHU12' # HUC12 WBD layer with ToHUC codes.
-wbd = gpd.read_file(wbdPth, layer = layer) # Load the WBD to a geodataframe.
+layer = 'WBDHU12'  # HUC12 WBD layer with ToHUC codes.
+wbd = gpd.read_file(wbdPth, layer=layer)  # Load the WBD to a geodataframe.
 
 # Convert HUC12 and ToHUC codes to 4-digit codes.,
 wbd['HUC4'] = wbd.HUC12.map(fcpg.getHUC4)
 wbd['ToHUC4'] = wbd.ToHUC.map(fcpg.getHUC4)
 
 # Find basins that contain pour points.
-pourBasins = fcpg.makePourBasins(wbd,upstream,downstream)
+pourBasins = fcpg.makePourBasins(wbd, upstream, downstream)
 
 # Find pour points between the upstream and downstream basins.
-pourPoints = fcpg.findPourPoints(pourBasins, upstreamFACfltau, upstreamFDRfltau, plotBasins = True)
+pourPoints = fcpg.findPourPoints(
+    pourBasins, upstreamFACfltau, upstreamFDRfltau, plotBasins=True)
 
 # Create update dictionary from the pour points.
-newX,newY,maxFAC = zip(*pourPoints)
-fcpg.createUpdateDict(newX,newY,maxFAC,upstream,updateDict)
+newX, newY, maxFAC = zip(*pourPoints)
+fcpg.createUpdateDict(newX, newY, maxFAC, upstream, updateDict)
 
 # Update the downstream basin using the update dictionary.
-fcpg.adjustFAC(downstreamFACfltau,downstreamFACWeight,updateDict,downstreamFDRfltau,downstreamAdjFAC, cores=cores) # note that this tool will create downstreamFACWeight if it does not exist based on downstreamFACfltau, see function documentation.
+# note that this tool will create downstreamFACWeight if it does not exist based on downstreamFACfltau, see function documentation.
+fcpg.adjustFAC(downstreamFACfltau, downstreamFACWeight, updateDict,
+               downstreamFDRfltau, downstreamAdjFAC, cores=cores)
