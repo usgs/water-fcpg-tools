f8308cbc7dca2a92e7d7c4f3d809e3516cd0d4ed
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-05-09 17:15:52 -0400

    Bug fix version increment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ copyright = f'2022, {author}'
 version = '2.0'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.3'
+release = '2.0.4'
 
 
 # -- General configuration ---------------------------------------------------
